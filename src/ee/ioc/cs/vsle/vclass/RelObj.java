8c1f316040e54a8fcba614e8ab036aa75eac8565
andrex42 <andrex42>
2006-11-01 06:57:00 +0000

    Draw selection marks on relation classes. Added some comments and annotations while at it.

--- src/ee/ioc/cs/vsle/vclass/RelObj.java
+++ src/ee/ioc/cs/vsle/vclass/RelObj.java
@@ -1,12 +1,14 @@
 package ee.ioc.cs.vsle.vclass;
 
+import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.Graphics2D;
 
 import ee.ioc.cs.vsle.graphics.Shape;
 import ee.ioc.cs.vsle.util.*;
 
 /**
-
+ * Relation class
  */
 public class RelObj extends GObj {
     private static final long serialVersionUID = 1L;
@@ -20,8 +22,10 @@ public class RelObj extends GObj {
 	}
 
 	public RelObj() {
+		// do nothing
 	}
 
+	@Override
 	public boolean contains(int pointX, int pointY) {
 		float f = VMath.pointDistanceFromLine(x, y, endX, endY, pointX, pointY);
 		if (f < height + 4) {
@@ -30,6 +34,7 @@ public class RelObj extends GObj {
 		return false;
 	}
 
+	@Override
 	void draw(int xPos, int yPos, float Xsize, float Ysize, Graphics2D g2) {
 		g2.translate(xPos, yPos);
 		g2.rotate(angle);
@@ -44,6 +49,7 @@ public class RelObj extends GObj {
 	} // draw
 
 
+	@Override
 	public void drawClassGraphics(Graphics2D g) {
 		draw(getX(), getY(), getXsize(), getYsize(), g);
 		int xModifier = getX();
@@ -61,8 +67,22 @@ public class RelObj extends GObj {
 					yModifier, getXsize(), getYsize(), g, field.getName(), field.value);
 			}
 		}
+
+		g.setColor(Color.black);
+		if (isSelected())
+			drawSelectionMarks(g);
 	}
 
+	private void drawSelectionMarks(Graphics g) {
+		g.fillRect(x - CORNER_SIZE / 2, y - CORNER_SIZE / 2,
+				CORNER_SIZE,  CORNER_SIZE);
+		g.fillRect((int) (x + width * Xsize * Math.cos(angle)) 
+				- CORNER_SIZE / 2,
+				(int) (y + width * Xsize * Math.sin(angle))
+				- CORNER_SIZE / 2, CORNER_SIZE, CORNER_SIZE);
+	}
+
+	@Override
 	public RelObj clone() {
 		RelObj obj = (RelObj) super.clone();
 		obj.startPort = startPort.clone();
@@ -70,6 +90,7 @@ public class RelObj extends GObj {
 		return obj;
 	}
 
+	@Override
 	public String toXML() {
 		String xml = "<relobject name=\"" + name + "\" type=\"" + className + "\" >\n";
 		xml += "  <relproperties x=\"" + x + "\" y=\"" + y + "\" endX=\"" + endX + "\" endY=\"" + endY + "\" angle=\"" + angle + "\" width=\"" + width + "\" height=\"" + height + "\" xsize=\"" + Xsize + "\" ysize=\"" + Ysize + "\" strict=\"" + strict + "\" />\n";
