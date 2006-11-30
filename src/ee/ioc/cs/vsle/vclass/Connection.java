7dd560d7021df646cacc8529f2dc1c881bd508a9
andrex42 <andrex42>
2006-11-30 14:20:35 +0000

    Better XML generation using TransformerHandler. Fixed adding breakpoints. Start and end points are no longer explicit breakpoint as it added redundancy that needed unnecessary bookkeeping.

--- src/ee/ioc/cs/vsle/vclass/Connection.java
+++ src/ee/ioc/cs/vsle/vclass/Connection.java
@@ -10,33 +10,53 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Stroke;
 
-/**
+import javax.xml.transform.sax.TransformerHandler;
 
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.AttributesImpl;
+
+/**
+ * Connection between two ports.
  */
 public class Connection implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 
 	/**
 	 * The stroke used for selected connections.
 	 */
 	public static final Stroke SELECTED_STROKE = new BasicStroke(3.0f);
 
+	/**
+	 * If a point is closer than {@code NEAR_DISTANCE} to this relation
+	 * line the point is considered to be close.
+	 */
+	public static final float NEAR_DISTANCE = 5.0f;
+	
 	public Port beginPort;
 	public Port endPort;
-	public int numOfBreakPoints = 0;
-	public boolean selected;
-	public ArrayList<Point> breakPoints = new ArrayList<Point>();
-	public static boolean objActive = false;
-
+	private boolean selected;
+	private ArrayList<Point> breakPoints;
+	
 	/**
-	 * Class constructor.
+	 * Is this an implicit connection between two strict ports 
+	 * that were connected by placing them close to each other.
 	 */
-	public Connection() {
-	} // ee.ioc.cs.editor.vclass.Connection
+	private boolean strict;
+
+	/**
+	 * Class constructor. The second port is left disconnected by
+	 * this constructor.
+	 * @param beginPort the first port the connection is connected to
+	 */
+	public Connection(Port beginPort) {
+		this.beginPort = beginPort;
+	}
 
 	/**
 	 * Class constructor.
-	 * @param beginPort ee.ioc.cs.editor.vclass.Port
-	 * @param endPort ee.ioc.cs.editor.vclass.Port
+	 * @param beginPort the first port the connection is connected to
+	 * @param endPort the second port the connection is connected to
 	 */
 	public Connection(Port beginPort, Port endPort) {
 		this.beginPort = beginPort;
@@ -44,23 +64,83 @@ public class Connection implements Serializable {
 	} // ee.ioc.cs.editor.vclass.Connection
 
 	/**
+	 * Constructs optionally a strict connection.
+	 * @param beginPort the first port the connection is connected to
+	 * @param endPort the second port the connection is connected to
+	 * @param strict if true the connections is strict
+	 */
+	public Connection(Port beginPort, Port endPort, boolean strict) {
+		this.beginPort = beginPort;
+		this.endPort = endPort;
+		this.strict = strict;
+	} // ee.ioc.cs.editor.vclass.Connection
+
+	/**
 	 * Find, if point is on or near the relation line.
-	 * @param pointX int - x coordinate of the point.
-	 * @param pointY int - y coordinate of the point.
-	 * @return float - distance between the point and the relation line.
+	 * @param pointX x coordinate of the point
+	 * @param pointY y coordinate of the point
+	 * @return distance between the point and the relation line
 	 */
 	float distanceFromPoint(int pointX, int pointY) {
-		Point p1;
-		Point p2;
-		float distance;
-		float minDistance = 100;
+		return distanceFromPoint(pointX, pointY, null);
+	}
 
-		for (int i = 0; i < breakPoints.size() - 1; i++) {
-			p1 = breakPoints.get(i);
-			p2 = breakPoints.get(i + 1);
-			distance = VMath.pointDistanceFromLine(p1.x, p1.y, p2.x, p2.y, pointX, pointY);
+	/**
+	 * Find, if point is on or near the relation line.
+	 * If {@code sindex} is non-{@code null} the index of the
+	 * closest line segment is returned as the first value in this array.
+	 * @param pointX x coordinate of the point.
+	 * @param pointY y coordinate of the point.
+	 * @param sindex an array that holds the line segment index returned
+	 * 		  from this method
+	 * @return distance between the point and the relation line.
+	 */
+	float distanceFromPoint(int pointX, int pointY, int[] sindex) {
+		float minDistance;
+
+		if (breakPoints == null || breakPoints.size() < 1) {
+			// there is only one line between the start and end points
+			minDistance = VMath.pointDistanceFromLine(beginPort.getAbsoluteX(),
+					beginPort.getAbsoluteY(), endPort.getAbsoluteX(),
+					endPort.getAbsoluteY(), pointX, pointY);
+			if (sindex != null)
+				sindex[0] = 0;
+		} else {
+			float distance;
+			Point p1 = breakPoints.get(0);
+			
+			// line from start point to the first breakpoint
+			minDistance = VMath.pointDistanceFromLine(beginPort.getAbsoluteX(),
+					beginPort.getAbsoluteY(), p1.x, p1.y, pointX, pointY);
+			if (sindex != null)
+				sindex[0] = 0;
+
+			Point p2;
+			
+			// lines between breapoints
+			for (int i = 0; i < breakPoints.size() - 1; i++) {
+				p1 = breakPoints.get(i);
+				p2 = breakPoints.get(i + 1);
+				distance = VMath.pointDistanceFromLine(p1.x, p1.y, p2.x, p2.y,
+						pointX, pointY);
+				if (distance < minDistance) {
+					minDistance = distance;
+					if (sindex != null)
+						sindex[0] = i + 1;
+				}
+			}
+
+			// line from last breakpoint to the end point
+			p2 = breakPoints.get(breakPoints.size() - 1);
+
+			distance = VMath.pointDistanceFromLine(p2.x, p2.y,
+					endPort.getAbsoluteX(), endPort.getAbsoluteY(),
+					pointX, pointY);
+
 			if (distance < minDistance) {
 				minDistance = distance;
+				if (sindex != null)
+					sindex[0] = breakPoints.size();
 			}
 		}
 
@@ -68,140 +148,84 @@ public class Connection implements Serializable {
 	} // distanceFromPoint
 
 	/**
-	 * Calculates the distance of a point from a line give by 2 points.
-	 * @param x1 int
-	 * @param y1 int
-	 * @param x2 int
-	 * @param y2 int
-	 * @param pointX int
-	 * @param pointY int
-	 * @return float
-	 */
-
-	/**
 	 * Adds a new breakpoint to the connection line.
-	 * The breakpoint locations are recalculated to cover the connection line equally.
+	 * The point is inserted at the specified index in the breakpoint list.
+	 * If the specified index is negative then the point is appended to
+	 * the list.
+	 * @param index the index where the point is inserted
+	 * @param p the point
 	 */
-	public void addBreakPoint() {
-		if (numOfBreakPoints < 2) {
-			numOfBreakPoints++;
-		}
-		calcEndBreakPoints();
+	public void addBreakPoint(int index, Point p) {
+		if (breakPoints == null)
+			breakPoints = new ArrayList<Point>();
+		
+		if (index < 0)
+			breakPoints.add(p);
+		else
+			breakPoints.add(index, p);
 	} // addBreakPoint
 
 	/**
-	 * Adds a new breakpoint to the connection line into a specific location.
+	 * Appends a new point to the list of breakpoints.
 	 * @param p Point
 	 */
 	public void addBreakPoint(Point p) {
+		if (breakPoints == null)
+			breakPoints = new ArrayList<Point>();
+		
 		breakPoints.add(p);
 	} // addBreakPoint
 
 	/**
-	 * Removes a breakpoint from the connection line.
+	 * Removes the last breakpoint from the connection line.
 	 */
 	public void removeBreakPoint() {
-		if (numOfBreakPoints > 0) {
-			numOfBreakPoints--;
-		}
-		calcEndBreakPoints();
+		if (breakPoints != null && breakPoints.size() > 0)
+			breakPoints.remove(breakPoints.size() - 1);
 	} // removeBreakPoint
 
 	/**
-	 * Calculates breakpoint locations to cover the connection line equally.
+	 * Removes a breakpoint that is near the specified point.
+	 * @param pointX X coordinate of the point
+	 * @param pointY Y coordinate of the point
+	 * @return <code>true</code> if a breakpoint was removed
 	 */
-	public void calcAllBreakPoints() {
-		Point p;
-		if (breakPoints.size() > 1) {
-			int port1X = beginPort.getX() + beginPort.getObject().getX();
-			int port1Y = beginPort.getY() + beginPort.getObject().getY();
-			int port2X = endPort.getX() + endPort.getObject().getX();
-			int port2Y = endPort.getY() + endPort.getObject().getY();
-			Point oldPoint1 = breakPoints.get(0);
-			int old1x = oldPoint1.x;
-			int old1y = oldPoint1.y;
+	public boolean removeBreakPoint(int pointX, int pointY) {
+		Point bp = breakPointContains(pointX, pointY);
+		if (bp != null)
+			return breakPoints.remove(bp);
+		
+		return false;
+	}
 
-			Point oldPoint2 = breakPoints.get(breakPoints.size()-1);
-			int old2x = oldPoint2.x;
-			int old2y = oldPoint2.y;
-
-			calcEndBreakPoints();
-
-			for (int i = 1; i < breakPoints.size()-1; i++) {
-
-				p = breakPoints.get(i);
-				p.x += ((port1X - old1x) + (port2X - old2x))/2;
-				p.y += ((port1Y - old1y) + (port2Y - old2y))/2;
-
-				/*
-				if (p.x == oldPoint1.x) {
-					p.x = port1X;
-				} else if (p.x == oldPoint2.x) {
-					p.x = port2X;
-				}
-				if (p.y == oldPoint1.y) {
-					p.y = port1Y;
-				} else if (p.y == oldPoint2.y) {
-					p.y = port2Y;
-				}*/
+	/**
+	 * Shifts the coordinates of the breakpoints by the specified amount. 
+	 * @param dx change of the X coordinate
+	 * @param dy change of the Y coordinate
+	 */
+	public void move(int dx, int dy) {
+		if (breakPoints != null) {
+			for (Point p : breakPoints) {
+				p.x += dx;
+				p.y += dy; 
 			}
-		}
-
-		/* if (numOfBreakPoints == 1) {
-		 if (beginPort.obj.getX()+beginPort.obj.getRealWidth() == port1X  || beginPort.obj.getX() == port1X) {
-		 break1X = port2X;
-		 break1Y = port1Y;
-		 } else {
-		 break1X = port1X;
-		 break1Y = port2Y;
-		 }
-		 } else if (numOfBreakPoints == 2) {
-		 if (beginPort.obj.getX()+ beginPort.obj.getRealWidth() == port1X  ||
-		 endPort.obj.getX()+ endPort.obj.getRealWidth() == port2X) {
-		 break1X = port1X + (port2X-port1X)/2;
-		 break1Y = port1Y;
-		 break2X = port1X + (port2X-port1X)/2;
-		 break2Y = port2Y;
-
-		 } else {
-		 break1X = port1X;
-		 break1Y = port1Y + ((port2Y-port1Y)/2);
-		 break2X = port2X ;
-		 break2Y = port1Y + ((port2Y-port1Y)/2);
-
-		 }
-		 }*/
-	} // calcBreakPoints
-
-	public void calcEndBreakPoints() {
-		if (breakPoints.size() > 1) {
-			int port1X = beginPort.getX() + beginPort.getObject().getX();
-			int port1Y = beginPort.getY() + beginPort.getObject().getY();
-			int port2X = endPort.getX() + endPort.getObject().getX();
-			int port2Y = endPort.getY() + endPort.getObject().getY();
-			Point oldPoint1 = breakPoints.get(0);
-			Point oldPoint2 = breakPoints.get(breakPoints.size() - 1);
-
-			oldPoint1.x += (port1X - oldPoint1.x);
-			oldPoint1.y += (port1Y - oldPoint1.y);
-
-			oldPoint2.x += (port2X - oldPoint2.x);
-			oldPoint2.y += (port2Y - oldPoint2.y);
-
-
 		}
 	}
 
 	/**
-	 * <UNCOMMENTED>
-	 * @param x int
-	 * @param y int
-	 * @return Point
+	 * Finds the breakpoint that contains the point (x, y).
+	 * @param x X coordinate of the point
+	 * @param y Y coordinate of the point
+	 * @return the breakpoint that contains (x, y); null if there
+	 * 		   is no such breakpoint.
 	 */
 	public Point breakPointContains(int x, int y) {
 		Point p;
 
-		for (int i = 1; i < breakPoints.size() - 1; i++) {
+		if (breakPoints == null)
+			return null;
+		
+		for (int i = 0; i < breakPoints.size(); i++) {
 			p = breakPoints.get(i);
 			if (x > p.x - 3 && x < p.x + 3 && y > p.y - 3 && y < p.y + 3) {
 				return p;
@@ -210,32 +234,137 @@ public class Connection implements Serializable {
 		return null;
 	} // breakPointContains
 
-	public String toXML() {
-		String xml = "<connection obj1=\"" + beginPort.getObject().getName() + "\" port1=\"" + beginPort +
-			"\" obj2=\"" + endPort.getObject().getName() + "\" port2=\"" + endPort + "\">\n";
-		xml += "  <breakpoints>\n";
-		for (Point point: breakPoints) {
-			xml += StringUtil.indent(4) + "<point x=\"" + point.x + "\" y=\"" + point.y + "\"/>\n";
-		}
-		xml += "  </breakpoints>\n";
-		xml += "</connection>\n";
-		return xml;
-
+	/**
+	 * Returns the list of breakpoints which can be empty or null if there
+	 * are no breakpoints. Start and end points are not counted as breakpoints.
+	 * @return the list of breakpoints
+	 */
+	public ArrayList<Point> getBreakPoints() {
+		return breakPoints;
 	}
 
 	/**
-	 * Draw the connection line.
+	 * Generates SAX events that are necessary to serialize this object to XML.
+	 * @param th the receiver of events
+	 * @throws SAXException
+	 */
+	public void toXML(TransformerHandler th) throws SAXException {
+		AttributesImpl attrs = new AttributesImpl();
+		
+		attrs.addAttribute(null, null, "obj1", StringUtil.CDATA,
+				beginPort.getObject().getName());
+		attrs.addAttribute(null, null, "port1", StringUtil.CDATA,
+				beginPort.toString());
+		attrs.addAttribute(null, null, "obj2", StringUtil.CDATA,
+				endPort.getObject().getName());
+		attrs.addAttribute(null, null, "port2", StringUtil.CDATA,
+				endPort.toString());
+
+		th.startElement(null, null, "connection", attrs);
+		th.startElement(null, null, "breakpoints", null);
+
+		if (breakPoints != null) {
+			for (Point point : breakPoints) {
+				attrs.clear();
+				attrs.addAttribute(null, null, "x", StringUtil.CDATA,
+						Integer.toString(point.x));
+				attrs.addAttribute(null, null, "y", StringUtil.CDATA,
+						Integer.toString(point.y));
+				th.startElement(null, null, "point", attrs);
+				th.endElement(null, null, "point");
+			}
+		}
+
+		th.endElement(null, null, "breakpoints");
+		th.endElement(null, null, "connection");
+	}
+
+	/**
+	 * Draw the connection line. The connection can be drawn only when
+	 * start and end ports are both present.
 	 * @param g Graphics
 	 */
 	public void drawRelation(Graphics2D g) {
+		drawRelation(g, beginPort.getAbsoluteX(), beginPort.getAbsoluteY(),
+				endPort.getAbsoluteX(), endPort.getAbsoluteY());
+	}
+
+	/**
+	 * Draw the connection line. The coordinates {@code endX}, {@code endY}
+	 * are used instead of the coordinates of the end port for drawing
+	 * the end point. This method can be used to draw connections with
+	 * disconnected end port.
+	 * @param g Graphics
+	 * @param endX X coordinate of the end point
+	 * @param endY Y coordinate of the end point
+	 */
+	public void drawRelation(Graphics2D g, int endX, int endY) {
+		drawRelation(g, beginPort.getAbsoluteX(), beginPort.getAbsoluteY(),
+				endX, endY);
+	}
+
+	/**
+	 * Draws the connection. The connection and breakpoints are hilighted
+	 * if the connection is selected.
+	 * @param g Graphics
+	 * @param startX X coordinate of the start point
+	 * @param startY Y coordinate of the start point
+	 * @param endX X coordinate of the end point
+	 * @param endY Y coordinate of the end point
+	 */
+	private void drawRelation(Graphics2D g, int startX, int startY,
+			int endX, int endY) {
+
+		// do not show strict and other connections that should be invisible
+		if (isImplicit())
+			return;
+		
 		Point p1, p2;
 
-		// hilight selected connections
-		if (selected) {
-			Stroke origStroke = g.getStroke();
-			Color origColor = g.getColor();
-			g.setStroke(SELECTED_STROKE);
-			g.setColor(Color.cyan);
+		if (breakPoints == null || breakPoints.size() < 1) {
+			// if there are no breakpoints there is only one line to draw
+			if (selected) {
+				// hilight selected connections
+				Stroke origStroke = g.getStroke();
+				Color origColor = g.getColor();
+				g.setStroke(SELECTED_STROKE);
+				g.setColor(Color.cyan);
+				
+				g.drawLine(startX, startY, endX, endY);
+				
+				g.setStroke(origStroke);
+				g.setColor(origColor);
+			}
+
+			g.drawLine(startX, startY, endX, endY);
+
+		} else { // there are breakpoints
+			if (selected) {
+				// hilight selected connections
+				Stroke origStroke = g.getStroke();
+				Color origColor = g.getColor();
+				g.setStroke(SELECTED_STROKE);
+				g.setColor(Color.cyan);
+
+				p1 = breakPoints.get(0);
+				g.drawLine(startX, startY, p1.x, p1.y);
+
+				for (int i = 0; i < breakPoints.size() - 1; i++) {
+					p1 = breakPoints.get(i);
+					p2 = breakPoints.get(i + 1);
+					g.drawLine(p1.x, p1.y, p2.x, p2.y);
+				}
+
+				p2 = breakPoints.get(breakPoints.size() - 1);
+				g.drawLine(p2.x, p2.y, endX, endY);
+				
+				g.setStroke(origStroke);
+				g.setColor(origColor);
+			}
+
+			// draw the connection lines
+			p1 = breakPoints.get(0);
+			g.drawLine(startX, startY, p1.x, p1.y);
 
 			for (int i = 0; i < breakPoints.size() - 1; i++) {
 				p1 = breakPoints.get(i);
@@ -243,26 +372,102 @@ public class Connection implements Serializable {
 				g.drawLine(p1.x, p1.y, p2.x, p2.y);
 			}
 
-			g.setStroke(origStroke);
-			g.setColor(origColor);
+			p2 = breakPoints.get(breakPoints.size() - 1);
+			g.drawLine(p2.x, p2.y, endX, endY);
+
+			// show breakpoints if selected
+			if (selected) {
+				for (Point p : breakPoints)
+					g.drawRect(p.x - 2, p.y - 2, 4, 4);
+			}
 		}
 
-		for (int i = 0; i < breakPoints.size() - 1; i++) {
-			p1 = breakPoints.get(i);
-			p2 = breakPoints.get(i + 1);
-			g.drawLine(p1.x, p1.y, p2.x, p2.y);
-		}
-		
 		if (selected) {
-			for (int i = 0; i < breakPoints.size(); i++) {
-				p1 = breakPoints.get(i);
-				g.drawRect(p1.x - 2, p1.y - 2, 4, 4);
-			}
+			// hilight start and end points
+			g.drawRect(startX - 2, startY - 2, 4, 4);
+			g.drawRect(endX - 2, endY - 2, 4, 4);
 		}
 	} // drawRelation
 
+	/**
+	 * Shows whether this connection was created implicitly.
+	 * Implicit connections are invisible and created and removed behind
+	 * the scenes.
+	 * @return <code>true</code> if this connection was created implicitly.
+	 */
+	public boolean isImplicit() {
+		return strict || isRelclass();
+	}
+
+	/**
+	 * Returns the number of breakpoints. Start and end points are
+	 * not counted as breakpoints.
+	 * @return the number of breakpoints.
+	 */
+	public int getBreakpointCount() {
+		return breakPoints == null ? 0 : breakPoints.size();
+	}
+
+	/**
+	 * Sets the selected value.
+	 * 
+	 * @param selected
+	 *            the value to be set
+	 */
+	public void setSelected(boolean selected) {
+		this.selected = selected;
+	}
+
+	/**
+	 * Returns selection status.
+	 * 
+	 * @return <code>true</code> if this connection is selected.
+	 */
 	public boolean isSelected() {
 		return selected;
 	}
 
+	/**
+	 * Is this connection an implicit connection between a relation class and an
+	 * ordinary class? Implicit connections are invisible.
+	 * 
+	 * @return <code>true</code> if this connection was created implicitly
+	 *         between a class and a relation class.
+	 */
+	public boolean isRelclass() {
+		// the fist port should always be present,
+		// the second port can be missing while the connection is being added
+		return (beginPort.getObject() instanceof RelObj 
+				|| (endPort != null && endPort.getObject() instanceof RelObj));
+	}
+
+	/**
+	 * Shows whether this connections is strict.
+	 * 
+	 * @return <code>true</code> if this connection is strict.
+	 */
+	public boolean isStrict() {
+		return strict;
+	}
+
+	/**
+	 * Finds the line segment being closest to the point and returns the index
+	 * of this line segment. If the point is not close enough to this connection
+	 * then -1 is returned.
+	 * 
+	 * @param pointX
+	 *            X coordinate of the point
+	 * @param pointY
+	 *            Y coordinate of the point
+	 * @return the 0-based index of the closest line segment, -1 if the point is
+	 *         not on the relation line
+	 */
+	public int indexOf(int pointX, int pointY) {
+		int[] sindex = new int[1];
+
+		if (distanceFromPoint(pointX, pointY, sindex) >= NEAR_DISTANCE)
+			sindex[0] = -1;
+
+		return sindex[0];
+	}
 }
