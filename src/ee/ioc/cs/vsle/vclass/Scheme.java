870e2d76d2a9ea761d234664578a7cd7a8a0e22d
andrex42 <andrex42>
2006-12-21 09:34:43 +0000

    Refactored visibility, lazy constructor, added reference to the package in the context of which the scheme has a meaning.

--- src/ee/ioc/cs/vsle/vclass/Scheme.java
+++ src/ee/ioc/cs/vsle/vclass/Scheme.java
@@ -8,22 +8,66 @@ import java.io.Serializable;
 public class Scheme implements Serializable {
 
     private static final long serialVersionUID = 1L;
-    public String packageName;
-	public ObjectList objects = new ObjectList();
-	public ConnectionList connections = new ConnectionList();
-	public String name;
+    private final VPackage vpackage;
+    private ObjectList objects;
+	private ConnectionList connections;
 
-	public Scheme(ObjectList objects, ConnectionList connections) {
+	public Scheme(VPackage vpackage, ObjectList objects,
+			ConnectionList connections) {
+
+		this.vpackage = vpackage;
 		this.objects = objects;
 		this.connections = connections;
 	}
 
-	public Scheme() {
-		// conctruct an empty scheme
+	public Scheme(VPackage vpackage) {
+		this.vpackage = vpackage;
 	}
 
 	@Override
 	public String toString() {
 		return "Objects: " + objects + " Connections: " + connections;
+	}
+
+	public VPackage getVPackage() {
+		return vpackage;
+	}
+
+	public String getPackageName() {
+		return vpackage.getName();
+	}
+
+	public ObjectList getObjects() {
+		if (objects == null)
+			objects = new ObjectList();
+
+		return objects;
+	}
+
+	public void setObjects(ObjectList objects) {
+		this.objects = objects;
+	}
+
+	public ConnectionList getConnections() {
+		if (connections == null)
+			connections = new ConnectionList();
+
+		return connections;
+	}
+
+	public void setConnections(ConnectionList connections) {
+		this.connections = connections;
+	}
+
+	/**
+	 * Returns the super class of the scheme if there is one.
+	 * @return the super class, null if there is none
+	 */
+	public GObj getSuperClass() {
+		for (GObj obj : objects)
+			if (obj.isSuperClass())
+				return obj;
+
+		return null;
 	}
 }
