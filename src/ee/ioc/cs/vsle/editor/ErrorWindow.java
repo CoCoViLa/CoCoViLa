448001ca48790fe3a73db233a68d6acdc6111650
andrex42 <andrex42>
2007-11-02 17:39:39 +0000

    Make the showErrorMessage() method thread safe; release native GUI resources by calling dispose() when the window is closed; try to automatically scroll to the locaton of first error message.

--- src/ee/ioc/cs/vsle/editor/ErrorWindow.java
+++ src/ee/ioc/cs/vsle/editor/ErrorWindow.java
@@ -2,60 +2,105 @@ package ee.ioc.cs.vsle.editor;
 
 import java.awt.*;
 
-import java.awt.event.*;
-
 import javax.swing.*;
+import javax.swing.text.BadLocationException;
 
-public class ErrorWindow extends JFrame implements ActionListener {
+import ee.ioc.cs.vsle.util.db;
 
-	JTextArea textArea;
-	JPanel errorText;
+public class ErrorWindow extends JFrame {
 
-        private static ErrorWindow instance = new ErrorWindow();
+    JTextArea textArea;
+    JPanel errorText;
+    JScrollPane areaScrollPane;
 
-        private ErrorWindow() {
-            super( "Error" );
+    private static ErrorWindow instance = new ErrorWindow();
 
-            addComponentListener( new ComponentResizer( ComponentResizer.CARE_FOR_MINIMUM ) );
+    private ErrorWindow() {
+        super( "Error" );
+
+        addComponentListener( new ComponentResizer( ComponentResizer.CARE_FOR_MINIMUM ) );
             
-            textArea = new JTextArea();
-            textArea.setFont( RuntimeProperties.getFont() );
-            JScrollPane areaScrollPane = new JScrollPane( textArea );
+        textArea = new JTextArea();
+        textArea.setFont( RuntimeProperties.getFont() );
+        areaScrollPane = new JScrollPane( textArea );
 
-            areaScrollPane.setVerticalScrollBarPolicy(
-                    JScrollPane.VERTICAL_SCROLLBAR_ALWAYS );
+        areaScrollPane.setVerticalScrollBarPolicy(
+                ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 
-            errorText = new JPanel();
-            errorText.setLayout( new BorderLayout() );
-            errorText.add( areaScrollPane, BorderLayout.CENTER );
+        errorText = new JPanel();
+        errorText.setLayout( new BorderLayout() );
+        errorText.add( areaScrollPane, BorderLayout.CENTER );
 
-            getContentPane().add( errorText );
-            textArea.setEditable( false );
-            textArea.setWrapStyleWord( true );
-            textArea.setLineWrap( true );
-            setDefaultCloseOperation(HIDE_ON_CLOSE);
-            setSize( 600, 300 );
-            validate();
+        getContentPane().add( errorText );
+        textArea.setEditable( false );
+        textArea.setWrapStyleWord( true );
+        textArea.setLineWrap( true );
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setSize( 600, 300 );
+        validate();
+    }
 
+    @Override
+    public void dispose() {
+        textArea.setText(null);
+        super.dispose();
+    }
+
+    /**
+     * Shows the specified message in a window.
+     * This method is thread safe.
+     * @param message the message to show
+     */
+    public static void showErrorMessage(final String message) {
+        if (SwingUtilities.isEventDispatchThread()) {
+            appendAndShow(message);
+        } else {
+            SwingUtilities.invokeLater(new Runnable() {
+               public void run() {
+                   appendAndShow(message);
+               }
+            });
+        }
+    }
+
+    /**
+     * Appends the specified message to the error window and brings the
+     * window to front.
+     * @param message the message to append
+     */
+    static void appendAndShow(String message) {
+        assert SwingUtilities.isEventDispatchThread();
+
+        int curLen = instance.textArea.getDocument().getLength();
+        int firstErrorIndex = message.indexOf("ERROR");
+
+        instance.textArea.append(message + "\n\n");
+        // The window must be set visible before modelToView() call,
+        // otherwise the size of the textarea is not computed yet.
+        instance.setVisible(true);
+
+        // The compiler generated error messages are often very long but
+        // the root cause of the error is described after the first occurence
+        // of the string "ERROR". Make it easier for the user to spot errors
+        // by scrolling to the hopefully most relevant location.
+        JViewport vp = instance.areaScrollPane.getViewport();
+        try {
+            Rectangle r = instance.textArea.modelToView(curLen 
+                    + firstErrorIndex);
+            Point p = vp.getViewPosition();
+            p.setLocation(0, r.y);
+            vp.setViewPosition(p);
+            // Hilight the first occurence of "ERROR"
+            if (firstErrorIndex > -1)
+                instance.textArea.select(curLen + firstErrorIndex,
+                        curLen + firstErrorIndex + 5);
+        } catch (BadLocationException e) {
+            db.p(e);
         }
 
-        public void setVisible( boolean value ) {
-            if( !value ) {
-                this.textArea.setText( "" );
-            }
-            super.setVisible( value );
+        if( instance.getState() == Frame.ICONIFIED ) {
+            instance.setState( Frame.NORMAL );
         }
-
-        public static void showErrorMessage( String message ) {
-            instance.textArea.append( message + "\n\n");
-
-            instance.setVisible( true );
-            if( instance.getState() == JFrame.ICONIFIED ) {
-                instance.setState( JFrame.NORMAL );
-            }
-            instance.toFront();
-        }
-
-	public void actionPerformed(ActionEvent e) {
-	}
+        instance.toFront();
+    }
 }
