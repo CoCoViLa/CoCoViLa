9d12a7deb7055dc7bbd896ea173a8a921f28d681
pgrigorenko <pgrigorenko>
2007-11-07 09:46:18 +0000

    removed BadLocationException stack trace printing if there are no ERROR messages

--- src/ee/ioc/cs/vsle/editor/ErrorWindow.java
+++ src/ee/ioc/cs/vsle/editor/ErrorWindow.java
@@ -71,33 +71,34 @@ public class ErrorWindow extends JFrame {
     static void appendAndShow(String message) {
         assert SwingUtilities.isEventDispatchThread();
 
-        int curLen = instance.textArea.getDocument().getLength();
-        int firstErrorIndex = message.indexOf("ERROR");
-
         instance.textArea.append(message + "\n\n");
         // The window must be set visible before modelToView() call,
         // otherwise the size of the textarea is not computed yet.
         instance.setVisible(true);
 
-        // The compiler generated error messages are often very long but
-        // the root cause of the error is described after the first occurence
-        // of the string "ERROR". Make it easier for the user to spot errors
-        // by scrolling to the hopefully most relevant location.
-        JViewport vp = instance.areaScrollPane.getViewport();
-        try {
-            Rectangle r = instance.textArea.modelToView(curLen 
-                    + firstErrorIndex);
-            Point p = vp.getViewPosition();
-            p.setLocation(0, r.y);
-            vp.setViewPosition(p);
-            // Hilight the first occurence of "ERROR"
-            if (firstErrorIndex > -1)
-                instance.textArea.select(curLen + firstErrorIndex,
-                        curLen + firstErrorIndex + 5);
-        } catch (BadLocationException e) {
-            db.p(e);
+        int firstErrorIndex = message.indexOf("ERROR");
+        
+        if( firstErrorIndex > -1 ) {
+        	int curLen = instance.textArea.getDocument().getLength();
+        	// The compiler generated error messages are often very long but
+        	// the root cause of the error is described after the first occurence
+        	// of the string "ERROR". Make it easier for the user to spot errors
+        	// by scrolling to the hopefully most relevant location.
+        	JViewport vp = instance.areaScrollPane.getViewport();
+        	try {
+        		Rectangle r = instance.textArea.modelToView(curLen 
+        				+ firstErrorIndex);
+        		Point p = vp.getViewPosition();
+        		p.setLocation(0, r.y);
+        		vp.setViewPosition(p);
+        		// Hilight the first occurence of "ERROR"
+        		instance.textArea.select(curLen + firstErrorIndex,
+        				curLen + firstErrorIndex + 5);
+        	} catch (BadLocationException e) {
+        		db.p(e);
+        	}
         }
-
+        
         if( instance.getState() == Frame.ICONIFIED ) {
             instance.setState( Frame.NORMAL );
         }
