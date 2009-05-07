71e267c90212fcf9bc650d2d102cb72fbf523731
andrex42 <andrex42>
2009-05-07 14:19:45 +0000

    Reusable code of Scheme Editor's palette moved into PaletteBase.

--- src/ee/ioc/cs/vsle/editor/Palette.java
+++ src/ee/ioc/cs/vsle/editor/Palette.java
@@ -2,7 +2,6 @@ package ee.ioc.cs.vsle.editor;
 
 import java.awt.*;
 import java.awt.event.*;
-import java.util.*;
 
 import javax.swing.*;
 import javax.swing.border.*;
@@ -14,207 +13,147 @@ import ee.ioc.cs.vsle.vclass.*;
  * Menubar that contains buttons for choosing tools
  * and classes from the current package.
  */
-public class Palette implements ActionListener {
+public class Palette extends PaletteBase {
 
-	/**
-	 * Allowed zoom levels of the scheme view. Should always contain
-	 * at least the value 1.0f.
-	 */
-	public static final float[] ZOOM_LEVELS
-			= { .1f, .2f, .5f, .75f, 1.0f, 1.5f, 2.0f, 4.0f };
+    protected Canvas canvas;
 
-	public static final Dimension BUTTON_SPACE = new Dimension(1, 0);
-	public static final Dimension PANEL_SPACE = new Dimension(5, 0);
-	public static final Insets BUTTON_BORDER = new Insets(2, 2, 2, 2);
+    public Palette(VPackage vPackage, final Canvas canv) {
+        super();
+        this.canvas = canv;
 
-	private JPanel toolBar;
-	private Canvas canvas;
-    private ArrayList<JToggleButton> buttons;
+        // Cannot use JToolBars here because it does not play nicely with changing
+        // L'n'F and scrollable toolbar. Hence buttons will look different 
+        // in CE and SE for now...
+        toolBar = new JPanel();
 
-    private final MouseListener mouseLst = new MouseAdapter() {
+        toolBar.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        toolBar.setOpaque(false); // there is transparent space between buttons
+        toolBar.setBorder(new EmptyBorder(2, 0, 2, 0));
 
-        @Override
-        public void mouseClicked( MouseEvent e ) {
-            if( SwingUtilities.isRightMouseButton( e ) ) {
-                int i = buttons.lastIndexOf( e.getSource() );
-                
-                if ( i < 0 ) {
-                    return;
-                }
-                
-                String action = buttons.get(i).getActionCommand();
-                
-                if( State.isAddObject( action ) || State.isAddRelClass( action ) ) {
-                    canvas.openClassCodeViewer( State.getClassName( action ) );
-                }
-            }
-        }
-    };
-    
-	public Palette(VPackage vPackage, final Canvas canv) {
-		this.canvas = canv;
+        c.gridx = 0;
+        toolBar.add(createToolPanel(), c);
 
-		buttons = new ArrayList<JToggleButton>();
+        c.gridx = 2;
+        toolBar.add(createZoomPanel(), c);
 
-		toolBar = new JPanel(new GridBagLayout());
-		GridBagConstraints c = new GridBagConstraints();
-		toolBar.setOpaque(false); // there is transparent space between buttons
-		toolBar.setBorder(new EmptyBorder(2, 0, 2, 0));
+        c.gridx = 1;
+        c.anchor = GridBagConstraints.LINE_START;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 1.0;
+        c.insets = new Insets(0, PANEL_SPACE.width, 0, PANEL_SPACE.width);
+        toolBar.add(new ScrollableBar(createClassPanel(vPackage)), c);
 
-		c.gridx = 0;
-		toolBar.add(createToolPanel(), c);
+        canvas.add(toolBar, BorderLayout.PAGE_START);
+    }
 
-		c.gridx = 2;
-		toolBar.add(createZoomPanel(), c);
+    private JComponent createClassPanel(VPackage vPackage) {
+        JPanel classPanel = new JPanel();
 
-		c.gridx = 1;
-		c.anchor = GridBagConstraints.LINE_START;
-		c.fill = GridBagConstraints.HORIZONTAL;
-		c.weightx = 1.0;
-		c.insets = new Insets(0, PANEL_SPACE.width, 0, PANEL_SPACE.width);
-		toolBar.add(new ScrollableBar(createClassPanel(vPackage)), c);
-
-		canvas.add(toolBar, BorderLayout.PAGE_START);
-	}
-
-	private JComponent createClassPanel(VPackage vPackage) {
-		JPanel classPanel = new JPanel();
-		classPanel.setLayout(new BoxLayout(classPanel, BoxLayout.LINE_AXIS));
-		classPanel.setOpaque(false);
+        classPanel.setOpaque(false);
+        classPanel.setBorder(new EmptyBorder(2, 0, 2, 0));
+        classPanel.setLayout(new BoxLayout(classPanel, BoxLayout.LINE_AXIS));
 
         // read package info and add it to the palette
-		for (int i = 0; i < vPackage.classes.size(); i++) {
-			PackageClass pClass = vPackage.classes.get(i);
+        for (int i = 0; i < vPackage.classes.size(); i++) {
+            PackageClass pClass = vPackage.classes.get(i);
 
-			ImageIcon icon;
-			if ("default.gif".equals(pClass.icon))
-				icon = FileFuncs.getImageIcon("images/default.gif", false);
-			else
-				icon = FileFuncs.getImageIcon(canvas.getWorkDir()
-						+ pClass.icon, true);
+            ImageIcon icon;
+            if ("default.gif".equals(pClass.icon)) {
+                icon = FileFuncs.getImageIcon("images/default.gif", false);
+            } else {
+                icon = FileFuncs.getImageIcon(canvas.getWorkDir()
+                        + pClass.icon, true);
+            }
 
-			String actionCmd;
-			if (pClass.relation == true)
-				// to denote a class which is a relation
-				actionCmd = State.addRelObjPrefix + pClass.name;
-			else
-				actionCmd = pClass.name;
+            String actionCmd;
+            if (pClass.relation == true) {
+                // to denote a class which is a relation
+                actionCmd = State.addRelObjPrefix + pClass.name;
+            } else {
+                actionCmd = pClass.name;
+            }
 
-			JToggleButton button = createButton(icon, pClass.name + " " + pClass.getDescription(),
-					actionCmd);
+            JToggleButton button = createButton(icon, pClass.name + " "
+                    + pClass.getDescription(), actionCmd);
 
-			classPanel.add(button);
+            classPanel.add(button);
 
-			if (i < vPackage.classes.size() - 1)
-				classPanel.add(Box.createRigidArea(BUTTON_SPACE));
-		}
-		return classPanel;
-	}
+            if (i < vPackage.classes.size() - 1) {
+                classPanel.add(Box.createRigidArea(BUTTON_SPACE));
+            }
+        }
+        return classPanel;
+    }
 
-	private JComponent createZoomPanel() {
-		JPanel zoomPanel = new JPanel();
-		zoomPanel.setLayout(new BoxLayout(zoomPanel, BoxLayout.LINE_AXIS));
-		zoomPanel.setOpaque(false);
-
-		JComboBox zoom = getZoomComboBox( RuntimeProperties.getZoomFactor() );
-
-		zoom.addActionListener(new ActionListener() {
-
-			public void actionPerformed(ActionEvent e) {
-				int i = ((JComboBox) e.getSource()).getSelectedIndex();
-				canvas.setScale(ZOOM_LEVELS[i]);
-			}
-
-		});
-		zoomPanel.add(zoom);
-
-		return zoomPanel;
-	}
-
-	private JComponent createToolPanel() {
+    private JComponent createToolPanel() {
         JPanel toolPanel = new JPanel();
         toolPanel.setLayout(new BoxLayout(toolPanel, BoxLayout.LINE_AXIS));
         toolPanel.setOpaque(false);
+        toolPanel.setBorder(new EmptyBorder(2, 0, 2, 0));
 
-		JToggleButton selection = createButton(
-				FileFuncs.getImageIcon("images/mouse.gif", false),
-				"Select / Drag", State.selection);
+        JToggleButton selection = createButton("images/mouse.gif",
+                "Select / Drag", State.selection);
 
-		selection.setSelected(true);
-		toolPanel.add(selection);
+        selection.setSelected(true);
+        toolPanel.add(selection);
 
-		toolPanel.add(Box.createRigidArea(BUTTON_SPACE));
+        toolPanel.add(Box.createRigidArea(BUTTON_SPACE));
 
-		JToggleButton relation = createButton(
-				FileFuncs.getImageIcon("images/rel.gif", false),
-				"Relation", State.addRelation);
+        JToggleButton relation = createButton("images/rel.gif",
+                "Relation", State.addRelation);
 
-		toolPanel.add(relation);
+        toolPanel.add(relation);
 
-		return toolPanel;
-	}
-
-	private JToggleButton createButton(ImageIcon icon, String descr,
-			String actionCmd) {
-
-		JToggleButton button = new JToggleButton(icon);
-
-		button.setActionCommand(actionCmd);
-		button.setToolTipText(descr);
-
-		// Palette buttons should be small, that's what JToolBar is doing
-		button.setMargin(BUTTON_BORDER);
-
-		button.addActionListener(this);
-		button.addMouseListener( mouseLst );
-        buttons.add(button);
-
-        return button;
-	}
-
-	/**
-	 * Creates and returns a new combo box for selecting a zoom level from
-	 * predefined zoom values.
-	 * @param selectedZoom the zoom value that should be selected by default
-	 * @return zoom combo box
-	 */
-	public static JComboBox getZoomComboBox(float selectedZoom) {
-		JComboBox zoom = new JComboBox();
-		for (int i = 0; i < ZOOM_LEVELS.length; i++) {
-			String label = Integer.toString((int) (ZOOM_LEVELS[i] * 100.0f));
-			zoom.addItem(label + "%");
-
-			if (ZOOM_LEVELS[i] == selectedZoom)
-				zoom.setSelectedIndex(i);
-		}
-
-		return zoom;
-	}
-
-	public void actionPerformed(ActionEvent e) {
-        int i = buttons.lastIndexOf(e.getSource());
-        if (i < 0)
-            return;
-
-        // ignore the click if the button _was_ already selected
-        JToggleButton button = buttons.get(i);
-        if (!button.isSelected()) {
-            button.setSelected(true);
-            return;
-        }
-		for (JToggleButton b: buttons) {
-		    if (b != button)
-		        b.setSelected(false);
-		}
-
-        canvas.mListener.setState(e.getActionCommand());
-
-        canvas.drawingArea.grabFocus();
-        canvas.drawingArea.repaint();
+        return toolPanel;
     }
 
-    void resetButtons() {
-        for (int i = 0; i < buttons.size(); i++)
-            buttons.get(i).setSelected(i == 0);
+    
+    @Override
+    protected MouseListener getButtonMouseListener() {
+        if (mouseListener == null) {
+            mouseListener = new MouseAdapter() {
+
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    if (SwingUtilities.isRightMouseButton(e)) {
+                        int i = buttons.lastIndexOf(e.getSource());
+
+                        if (i < 0) {
+                            return;
+                        }
+
+                        String action = buttons.get(i).getActionCommand();
+
+                        if (State.isAddObject(action) 
+                                || State.isAddRelClass(action)) {
+
+                            canvas.openClassCodeViewer(
+                                    State.getClassName(action ));
+                        }
+                    }
+                }
+            };
+        }
+        return mouseListener;
+    }
+
+    @Override
+    protected ActionListener getZoomListener() {
+        return new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                int i = ((JComboBox) e.getSource()).getSelectedIndex();
+                canvas.setScale(ZOOM_LEVELS[i]);
+            }
+
+        };
+    }
+
+    @Override
+    protected void setState(String state) {
+        canvas.mListener.setState(state);
+        canvas.drawingArea.grabFocus();
+        canvas.drawingArea.repaint();
     }
 }
