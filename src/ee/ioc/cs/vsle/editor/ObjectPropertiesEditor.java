c7dfb652b4c0e74e55524c86d0e1b637c6da08d5
pgrigorenko <pgrigorenko>
2009-10-09 07:05:35 +0000

    Fixed bug#2875526. - removed code duplication - removed combos for aliases - added Object fields - removed old commented code related to watch functionality

--- src/ee/ioc/cs/vsle/editor/ObjectPropertiesEditor.java
+++ src/ee/ioc/cs/vsle/editor/ObjectPropertiesEditor.java
@@ -20,7 +20,6 @@ public class ObjectPropertiesEditor extends JFrame implements ActionListener,
 	
     private static final long           serialVersionUID  = 1L;
     private ArrayList<JTextField>       textFields        = new ArrayList<JTextField>();
-//    private ArrayList<JCheckBox>        watchFields       = new ArrayList<JCheckBox>();
     private ArrayList<JCheckBox>        goalInputs            = new ArrayList<JCheckBox>();
     private ArrayList<JCheckBox>        goalOutputs             = new ArrayList<JCheckBox>();
     private ArrayList<JComboBox>        comboBoxes        = new ArrayList<JComboBox>();
@@ -110,14 +109,12 @@ public class ObjectPropertiesEditor extends JFrame implements ActionListener,
 		addComponentListener( lst );
 		
 		JPanel labelPane = new JPanel();
-//		JPanel watchPane = new JPanel();
 		JPanel inputPane = new JPanel();
 		JPanel outputPane = new JPanel();
 		JPanel textFieldPane = new JPanel();
 		JPanel typePane = new JPanel();
 
 		labelPane.setLayout(new GridLayout(0, 1));
-//		watchPane.setLayout(new GridLayout(0, 1));
 		inputPane.setLayout(new GridLayout(0, 1));
 		outputPane.setLayout(new GridLayout(0, 1));
 		typePane.setLayout(new GridLayout(0, 1));
@@ -136,16 +133,21 @@ public class ObjectPropertiesEditor extends JFrame implements ActionListener,
 		
 		label = new JLabel( "  Output  ", SwingConstants.CENTER );
 		outputPane.add(label);
-		
-//		label = new JLabel( "  Watch", SwingConstants.CENTER );
-//		watchPane.add(label);
 
 		JTextField textField;
 		JComboBox comboBox;
-//		JCheckBox watch;
 		
 		for (ClassField field : object.getFields()) {
-			if (field.isArray()) {
+
+            String name = field.isAlias() ? "alias " : "";
+            name += field.getName();
+            label = new JLabel(name, SwingConstants.CENTER);
+            label.setToolTipText(field.getDescription());
+            labelPane.add(label);
+            label = new JLabel("(" + field.getType() + ")");
+            typePane.add(label);
+
+            if (field.isArray() && !field.isAlias()) {
 				comboBox = new JComboBox();
 				comboBox.setEditable(true);
 				comboBox.addActionListener(this);
@@ -159,52 +161,33 @@ public class ObjectPropertiesEditor extends JFrame implements ActionListener,
 				}
 				Component jtf = comboBox.getEditor().getEditorComponent();
 				jtf.addKeyListener(this);
-
 				comboBoxes.add(comboBox);
-				label = new JLabel(field.getName(), SwingConstants.CENTER);
-				label.setToolTipText(field.getDescription());
 				arrayNameList.add(field);
-				labelPane.add(label);
-				label = new JLabel("(" + field.getType() + ")");
-				typePane.add(label);
-
-//				watch = new JCheckBox();
-//				watch.setHorizontalAlignment( SwingConstants.CENTER );
-//				watch.setEnabled(false);
-//				watchPane.add(watch);
-//				watchFields.add(watch);
-
 				textFieldPane.add(comboBox);
 			} else if (field.isPrimitiveOrString()) {
 				textField = new JTextField();
 				textField.addKeyListener(this);
 				textField.setName(field.getName());
-				primitiveNameList.add(field);
 				textField.setText(field.getValue());
+                textFieldPane.add(textField);
+				primitiveNameList.add(field);
 				textFields.add(textField);
-				label = new JLabel(field.getName(), SwingConstants.CENTER);
-				label.setToolTipText(field.getDescription());
-				labelPane.add(label);
-				textFieldPane.add(textField);
-				label = new JLabel("(" + field.getType() + ")");
-				typePane.add(label);
-//				boolean b = field.isWatched();
-//				watch = new JCheckBox((String)null, b);
-//				watch.setHorizontalAlignment( SwingConstants.CENTER );
-//				watchPane.add(watch);
-//				watchFields.add(watch);
+			} else {
+			    textField = new JTextField();
+			    textField.setEditable( false );
+                textField.setName(field.getName());
+                textFieldPane.add(textField);
 			}
 			
-			boolean enable = field.isPrimitiveOrString();//TODO - tmp until combos are refactored
-			
 			final JCheckBox input = new JCheckBox( (String)null, field.isInput() );
-			input.setEnabled( enable );
+			//TODO only input for primitives and strings are supported by the gui
+			input.setEnabled( field.isPrimitiveOrString() );
 			input.setHorizontalAlignment( SwingConstants.CENTER );
 			inputPane.add(input);
 			goalInputs.add(input);
 			
 			final JCheckBox output = new JCheckBox( (String)null, field.isGoal() );
-			output.setEnabled( enable );
+			output.setEnabled( true );
 			output.setHorizontalAlignment( SwingConstants.CENTER );
 			outputPane.add(output);
 			goalOutputs.add(output);
@@ -231,7 +214,6 @@ public class ObjectPropertiesEditor extends JFrame implements ActionListener,
 		isStatic = new JCheckBox( "Static", object.isStatic() );
 		textFieldPane.add(isStatic);
 		labelPane.add( new JLabel() );
-//		watchPane.add( new JLabel() );
 		inputPane.add( new JLabel() );
 		outputPane.add( new JLabel() );
 		typePane.add( new JLabel() );
@@ -254,7 +236,6 @@ public class ObjectPropertiesEditor extends JFrame implements ActionListener,
 		goalPane.add(inputPane);
 		goalPane.add(outputPane);
 		eastPanel.add(goalPane);
-//		eastPanel.add(watchPane);
 		JPanel propsPane = new JPanel(new BorderLayout());
 		propsPane.setBorder( BorderFactory.createTitledBorder( "Properties" ) );
 		propsPane.add( westPanel, BorderLayout.WEST );
@@ -369,14 +350,10 @@ public class ObjectPropertiesEditor extends JFrame implements ActionListener,
 		    int txtIdx = 0;
 
 		    for (ClassField fld : controlledObject.getFields()) {
-		        if (fld.isArray()) {
-		            fld.setInput(goalInputs.get(i).isSelected());
-		            fld.setGoal(goalOutputs.get(i).isSelected());
-		            i++;
-		        } else if (fld.isPrimitiveOrString()) { 
-//		            fld.setWatched(watchFields.get(i).isSelected());
-		            fld.setInput(goalInputs.get(i).isSelected());
-		            fld.setGoal(goalOutputs.get(i).isSelected());
+		        fld.setInput(goalInputs.get(i).isSelected());
+		        fld.setGoal(goalOutputs.get(i).isSelected());
+		            
+		        if (fld.isPrimitiveOrString()) { 
 		            textField = textFields.get(txtIdx);
 		            if (!textField.getText().trim().equals("")) {
 		                fld.setValue(textField.getText());
@@ -384,8 +361,8 @@ public class ObjectPropertiesEditor extends JFrame implements ActionListener,
 		                fld.setValue(null);
 		            }
 		            txtIdx++;
-		            i++;
 		        }
+		        i++;
 		    }
 
 		    for (i = 0; i < comboBoxes.size(); i++) {
