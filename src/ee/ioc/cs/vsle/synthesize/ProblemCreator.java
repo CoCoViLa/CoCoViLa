CoCoViLa/CoCoViLa

commit 0169d0d23f61a9a94b6c87e4946162314329dcf1

Solved some performance issues: redesigned problem context copying in planning procedure

src/ee/ioc/cs/vsle/synthesize/ProblemCreator.java
@@ -107,7 +107,7 @@ public class ProblemCreator {
             }
             
             if( cf.isConstant() ) {
-                problem.getKnownVars().add( var );
+                problem.getCurrentContext().getKnownVars().add( var );
             }
             
             if( cf.isAliasLength() && cf.getValue() == null ) {
@@ -263,7 +263,9 @@ public class ProblemCreator {
 
                 } 
             } else if( relSet != null ) {
-                problem.addAllRels( relSet );
+                for ( Rel _rel : relSet ) {
+                    problem.addRel( _rel );
+                }
             }
 
         }
@@ -554,7 +556,7 @@ public class ProblemCreator {
         
         //if an alias has been initialized with an empty list of elements it is always computable
         if( var.getChildVars().isEmpty() && alias.isInitialized() ) {
-            problem.getKnownVars().add( var );
+            problem.getCurrentContext().getKnownVars().add( var );
         }
     }
     
@@ -780,7 +782,7 @@ public class ProblemCreator {
                 }
                 
                 if( newAliasVar.getChildVars().isEmpty() ) {
-                    problem.getKnownVars().add( newAliasVar );
+                    problem.getCurrentContext().getKnownVars().add( newAliasVar );
                 }
                 
                 aliasElements.put(varName, newAliasVar);
@@ -958,12 +960,14 @@ public class ProblemCreator {
       {//Assumptions
           Collection<Var> flattened = new HashSet<Var>();
           CodeGenerator.unfoldVarsToSet( goal.getInputs(), flattened );
-          problem.getKnownVars().addAll( flattened );
+          problem.getCurrentContext().getKnownVars().addAll( flattened );
           problem.getAssumptions().addAll( flattened );
       }
       
       {//Goals
-          CodeGenerator.unfoldVarsToSet( goal.getOutputs(), problem.getGoals() );
+          Set<Var> goals = new LinkedHashSet<Var>();
+          CodeGenerator.unfoldVarsToSet( goal.getOutputs(), goals );
+          problem.getCurrentContext().addGoals( goals );
       }
 
   }

src/ee/ioc/cs/vsle/synthesize/AlgorithmVisualizer.java
@@ -56,8 +56,8 @@ public class AlgorithmVisualizer extends JFrame implements TextEditView {
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
 				AlgorithmVisualizer thisClass = getInstance();
-				thisClass.addNewTab( "test", new ArrayList<Rel>() );
-				thisClass.addNewTab( "test2", new ArrayList<Rel>() );
+//				thisClass.addNewTab( "test", new ArrayList<Rel>() );
+//				thisClass.addNewTab( "test2", new ArrayList<Rel>() );
 				thisClass.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 				thisClass.setVisible(true);
 			}
@@ -94,7 +94,7 @@ public class AlgorithmVisualizer extends JFrame implements TextEditView {
 	 * @param title
 	 * @param algorithm
 	 */
-	public void addNewTab( String title, List<Rel> algorithm ) {
+	public void addNewTab( String title, EvaluationAlgorithm algorithm ) {
 		
 		final TabPanel panel = new TabPanel( algorithm );
 		
@@ -161,7 +161,7 @@ public class AlgorithmVisualizer extends JFrame implements TextEditView {
 
 		private JScrollPane jScrollPane = null;
 		
-		TabPanel( final List<Rel> algorithm ) {
+		TabPanel( final EvaluationAlgorithm algorithm ) {
 			setLayout(new BorderLayout());
 			add(getJScrollPane(), BorderLayout.CENTER);
 			
@@ -212,12 +212,13 @@ public class AlgorithmVisualizer extends JFrame implements TextEditView {
 			return jScrollPane;
 		}
 		
-		private String generateAlgorithmText( List<Rel> algorithm ) {
+		private String generateAlgorithmText( EvaluationAlgorithm algorithm ) {
 			
 			StringBuilder alg = new StringBuilder();
 
-			for ( Rel rel : algorithm ) {
-
+			for ( PlanningResult res : algorithm ) {
+			    Rel rel = res.getRel();
+			    
 				String obj = rel.getParentObjectName();
 				
 				if( TypeUtil.TYPE_THIS.equals( obj ) ) {
@@ -238,7 +239,7 @@ public class AlgorithmVisualizer extends JFrame implements TextEditView {
 						alg.append("!Subtask ");
 						alg.append(subtask.getDeclaration());
 						alg.append(" :\n");
-						alg.append(generateAlgorithmText(subtask.getAlgorithm()));
+						alg.append(generateAlgorithmText(res.getSubtaskAlgorithm( subtask )));
 						left();
 					}
 

src/ee/ioc/cs/vsle/synthesize/ClassRelation.java
@@ -16,8 +16,7 @@ import java.util.regex.Matcher;
  * @author  Ando Saabas
  * @version 1.0
  */
-class ClassRelation
-	implements Serializable {
+class ClassRelation  {
 
     /**
      * 

src/ee/ioc/cs/vsle/synthesize/CodeGenerator.java
@@ -24,13 +24,13 @@ public class CodeGenerator {
         OT_INC, OT_DEC
     }
 
-    private int                      subCount            = 0;
+    private int subCount = 0;
 
-    public static int                ALIASTMP_NR;
+    public static int ALIASTMP_NR;
 
-    private List<Rel>                algRelList;
-    private Problem                  problem;
-    private String                   className;
+    private EvaluationAlgorithm algorithm;
+    private Problem problem;
+    private String className;
 
     private Map<SubtaskRel, IndSubt> independentSubtasks = new HashMap<SubtaskRel, IndSubt>();
 
@@ -40,10 +40,10 @@ public class CodeGenerator {
         offset = "";
     }
     
-    public CodeGenerator( List<Rel> algRelList, Problem problem, String className ) {
+    public CodeGenerator( EvaluationAlgorithm algorithm, Problem problem, String className ) {
         
         this();
-        this.algRelList = algRelList;
+        this.algorithm = algorithm;
         this.problem = problem;
         this.className = className;
     }
@@ -63,8 +63,10 @@ public class CodeGenerator {
 
         genInputs( alg, problem.getAssumptions(), COMPUTE_ARG_NAME );
 
-        for ( Rel rel : algRelList ) {
+        for ( PlanningResult res : algorithm ) {
 
+            Rel rel = res.getRel();
+            
             if ( rel.getType() != RelType.TYPE_METHOD_WITH_SUBTASK ) {
                 appendRelToAlg( rel.toString(), rel.getExceptions(), alg, false );
             }
@@ -76,7 +78,7 @@ public class CodeGenerator {
                 unfoldVarsToSet( rel.getInputs(), usedVars );
                 unfoldVarsToSet( rel.getOutputs(), usedVars );
 
-                genRelWithSubtasks( rel, alg, false, className, usedVars, problem );
+                genRelWithSubtasks( res, alg, false, className, usedVars, problem );
             }
 
         }
@@ -111,8 +113,9 @@ public class CodeGenerator {
      * @param usedVars
      * @param _problem
      */
-    private void genRelWithSubtasks( Rel rel, StringBuilder alg, boolean isNestedSubtask, String parentClassName, Set<Var> usedVars, Problem _problem ) {
+    private void genRelWithSubtasks( PlanningResult res, StringBuilder alg, boolean isNestedSubtask, String parentClassName, Set<Var> usedVars, Problem _problem ) {
 
+        Rel rel = res.getRel();
         String relString = rel.toString();
         
         for ( SubtaskRel subtask : rel.getSubtasks() ) {
@@ -124,7 +127,7 @@ public class CodeGenerator {
             String sbClassName;
             if ( !subtask.isIndependent() || ( subtask.isIndependent() && !independentSubtasks.containsKey( subtask ) ) ) {
                 
-                sbClassName = genSubtask( bufSbtClass, parentClassName, usedVars, _problem,
+                sbClassName = genSubtask( res.getSubtaskAlgorithm( subtask ), bufSbtClass, parentClassName, usedVars, _problem,
                         subtask, subNum );
                 
                 if ( subtask.isIndependent() ) {
@@ -161,7 +164,7 @@ public class CodeGenerator {
      * @param subNum
      * @return
      */
-    private String genSubtask( StringBuilder bufSbtClass, String parentClassName,
+    private String genSubtask( EvaluationAlgorithm subAlg, StringBuilder bufSbtClass, String parentClassName,
             Set<Var> usedVars, Problem _problem,
             SubtaskRel subtask, int subNum ) {
         
@@ -193,19 +196,19 @@ public class CodeGenerator {
         unfoldVarsToSet( subInputs, currentUsedVars );
         unfoldVarsToSet( subOutputs, currentUsedVars );
 
-        List<Rel> subAlg = subtask.getAlgorithm();
         right();
         bufSbtBody.append( same() ).append( "//Subtask: " ).append( subtask ).append( "\n" );
         // apend subtask inputs to algorithm
         genInputs( bufSbtBody, subInputs, subtaskInputArrayName );
 
         for ( int i = 0; i < subAlg.size(); i++ ) {
-            Rel trel = subAlg.get( i );
+            PlanningResult res = subAlg.get( i );
+            Rel trel = res.getRel();
             if ( RuntimeProperties.isLogDebugEnabled() )
                 db.p( "rel " + trel + " in " + trel.getInputs() + " out " + trel.getOutputs() );
             if ( trel.getType() == RelType.TYPE_METHOD_WITH_SUBTASK ) {
                 // recursion
-                genRelWithSubtasks( trel, bufSbtBody, true, sbClassName, currentUsedVars, currentProblem );
+                genRelWithSubtasks( res, bufSbtBody, true, sbClassName, currentUsedVars, currentProblem );
 
             } else {
                 appendRelToAlg( trel.toString(), trel.getExceptions(), bufSbtBody, true );
@@ -244,9 +247,9 @@ public class CodeGenerator {
      * @param classCode
      * @return name of the generated class
      */
-    static String genIndependentSubtask(SubtaskRel subtask, StringBuilder classCode) {
+    static String genIndependentSubtask(SubtaskRel subtask, EvaluationAlgorithm subAlg, StringBuilder classCode) {
         
-        return new CodeGenerator().genSubtask( classCode, null, null, null, subtask, 0 );
+        return new CodeGenerator().genSubtask( subAlg, classCode, null, null, null, subtask, 0 );
     }
     
     private static String _this_ = "." + TYPE_THIS + ".";

src/ee/ioc/cs/vsle/synthesize/EvaluationAlgorithm.java
@@ -1,0 +1,19 @@
+/**
+ * 
+ */
+package ee.ioc.cs.vsle.synthesize;
+
+import java.util.*;
+
+/**
+ * Evaluation Algorithm is a list of relations wrapped
+ * into PlanningResult objects.
+ * 
+ * @author pavelg
+ */
+public class EvaluationAlgorithm extends ArrayList<PlanningResult> {
+
+    public void addRel(Rel rel) {
+        add(new PlanningResult( rel ));
+    }
+}

src/ee/ioc/cs/vsle/synthesize/IPlanner.java
@@ -1,10 +1,9 @@
 package ee.ioc.cs.vsle.synthesize;
 
-import java.awt.Component;
-import java.util.*;
+import java.awt.*;
 
 public interface IPlanner {
-	public ArrayList<Rel> invokePlaning( Problem problem, boolean computeAll );
+	public EvaluationAlgorithm invokePlaning( Problem problem, boolean computeAll );
 	
 	public String getDescription();
 	

src/ee/ioc/cs/vsle/synthesize/Optimizer.java
@@ -16,13 +16,14 @@ public class Optimizer {
 	 @param algorithm an unoptimized algorithm
 	 @param goals the variables which the algorithm has to calculate (other branches are removed)
 	 */   
-    public static void optimize( Problem problem, List<Rel> algorithm, Set<Var> goals ) {
-    	optimize( problem, algorithm, new HashSet<Var>( goals ), "" );
+    public static void optimize( PlanningContext context, EvaluationAlgorithm algorithm ) {
+    	optimize( context, algorithm, new HashSet<Var>( context.getAllGoals() ), "" );
     }
     
-	private static void optimize( Problem problem, List<Rel> algorithm, Set<Var> goals, String p ) {
+	private static void optimize( PlanningContext context, EvaluationAlgorithm algorithm, Set<Var> goals, String p ) {
 		Rel rel;
-		ArrayList<Rel> removeThese = new ArrayList<Rel>();
+		PlanningResult res;
+		EvaluationAlgorithm removeThese = new EvaluationAlgorithm();
 		
 		if (RuntimeProperties.isLogDebugEnabled())
 			db.p( p + "!!!--------- Starting Optimization with targets: " + goals + " ---------!!!");
@@ -30,7 +31,9 @@ public class Optimizer {
 		for (int i = algorithm.size() - 1; i >= 0; i--) {
 			if (RuntimeProperties.isLogDebugEnabled())
 				db.p( p + "Reguired vars: " + goals );
-            rel = algorithm.get(i);
+			
+			res = algorithm.get(i);
+            rel = res.getRel();
             if (RuntimeProperties.isLogDebugEnabled())
     			db.p( p + "Rel from algorithm: " + rel );
 			boolean relIsNeeded = false;
@@ -57,7 +60,7 @@ public class Optimizer {
 						HashSet<Var> subGoals = new HashSet<Var>();
 						CodeGenerator.unfoldVarsToSet( subtask.getOutputs(), subGoals );
 						// the problem object is required only on the top level
-						optimize( null, subtask.getAlgorithm(), subGoals, incPrefix( p ) );
+						optimize( null, res.getSubtaskAlgorithm( subtask ), subGoals, incPrefix( p ) );
 						if (RuntimeProperties.isLogDebugEnabled()) {
 							db.p( p + "Finished optimizing subtask: " + subtask );
 							db.p( p + "Required inputs from upper level: " + subGoals );
@@ -73,7 +76,7 @@ public class Optimizer {
 			} else {
 				if (RuntimeProperties.isLogDebugEnabled())
 					db.p( p + "Removed");
-				removeThese.add(rel);
+				removeThese.add(res);
 			}
 		}
 		if (RuntimeProperties.isLogDebugEnabled()) {
@@ -81,18 +84,18 @@ public class Optimizer {
 		}
 		
 		//remove unneeded relations
-		for (Rel relToRemove : removeThese) {
-			if( algorithm.indexOf( relToRemove ) > -1 ) {
-				algorithm.remove( relToRemove);
+		for (PlanningResult resToRemove : removeThese) {
+			if( algorithm.indexOf( resToRemove ) > -1 ) {
+				algorithm.remove( resToRemove);
 			}
 			
-			if( problem != null ) {
+			if( context != null ) {
 			    /* 
 			     * Do not keep vars in Found set if a relation that introduces 
 			     * those vars has been removed, otherwise the propagation procedure
 			     * may overwrite values of such variables.
 			     */
-			    problem.getFoundVars().removeAll( relToRemove.getOutputs() );
+			    context.getFoundVars().removeAll( resToRemove.getRel().getOutputs() );
 			}
 		}
 		if (RuntimeProperties.isLogDebugEnabled())

commit 97c3230969ac6261048d2c38b771143362123517

Fixed Bug:3012407 "Regression in commit 0169d0d2"

src/ee/ioc/cs/vsle/synthesize/ProblemCreator.java
@@ -756,18 +756,16 @@ public class ProblemCreator {
                 Var newAliasVar = new Var( newAlias, parent );
                 problem.addVar( newAliasVar );
                 
-                Rel aliasInputRel = new Rel(parent, newAlias.getName() + " ->");
+                Rel aliasInputRel = new Rel(parent, "(" + varName + ") " + newAlias.getName() + " ->");
                 aliasInputRel.addInput( newAliasVar );
                 newAliasVar.addRel( aliasInputRel );
                 aliasInputRel.setMethod( TypeUtil.TYPE_ALIAS );
                 aliasInputRel.setType( RelType.TYPE_ALIAS );
-                problem.addRel( aliasInputRel );
                 
-                Rel aliasOutputRel = new Rel(parent, "-> " + newAlias.getName());
+                Rel aliasOutputRel = new Rel(parent, "-> " + "(" + varName + ") " + newAlias.getName());
                 aliasOutputRel.addOutput( newAliasVar );
                 aliasOutputRel.setMethod( TypeUtil.TYPE_ALIAS );
                 aliasOutputRel.setType( RelType.TYPE_ALIAS );
-                problem.addRel( aliasOutputRel );
                 
                 int i = 0;
                 for ( Var elementVar : aliasVar.getChildVars() ) {
@@ -780,6 +778,9 @@ public class ProblemCreator {
                     aliasOutputRel.addInput( var );
                     var.addRel( aliasOutputRel );
                 }
+                
+                problem.addRel( aliasInputRel );
+                problem.addRel( aliasOutputRel );
                 
                 if( newAliasVar.getChildVars().isEmpty() ) {
                     problem.getCurrentContext().getKnownVars().add( newAliasVar );