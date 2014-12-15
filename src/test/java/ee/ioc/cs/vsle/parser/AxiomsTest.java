4e9c550756af766346cf6bedbc1f6461d3fe018b
Pavel Grigorenko <pgrigorenko@users.sourceforge.net>
2014-12-15 09:14:28 +0200

    Tests for subtasks and goals

--- src/test/java/ee/ioc/cs/vsle/parser/AxiomsTest.java
+++ src/test/java/ee/ioc/cs/vsle/parser/AxiomsTest.java
@@ -4,6 +4,8 @@ import ee.ioc.cs.vsle.synthesize.*;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Iterator;
+
 import static org.junit.Assert.*;
 
 /**
@@ -57,7 +59,6 @@ public class AxiomsTest extends AbstractParserTest {
     assertClassRelation(ac, RelType.TYPE_JAVAMETHOD, vars("a"), vars("b"), vars("Exception", "RuntimeException"), "m");
   }
 
-  //[z->y],a -> b {methodName};		//VALID
   @Test
   public void testAxiom_withSubtask() {
     String spec = "int a, b, x, y;\n [x -> y], a -> b {m};";
@@ -69,8 +70,74 @@ public class AxiomsTest extends AbstractParserTest {
     assertFalse("Should not be independent", subtask.isIndependent());
   }
 
-  //[z,x->y,u],a -> b {methodName};	//VALID
-//  [z->y] -> b.ou {methodName};	//VALID
+  @Test
+  public void testAxiom_withSubtaskMoreVars() {
+    specificationSourceProvider.add("M", wrapSpec( "int n, m, k, l;\n" ));
+    String spec = "M a, b, x, y;\n [x.n, u.m -> y.k, v.l], a.n -> b.m {m};";
+    AnnotatedClass ac = loadSpec(spec);
+    ClassRelation cr = assertClassRelation(ac, RelType.TYPE_METHOD_WITH_SUBTASK, vars("a.n"), vars("b.m"), "m");
+    assertEquals(1, cr.getSubtasks().size());
+    SubtaskClassRelation subtask = cr.getSubtasks().iterator().next();
+    assertClassRelation(subtask, RelType.TYPE_SUBTASK, vars("x.n", "u.m"), vars("y.k", "v.l"), null);
+    assertFalse("Should not be independent", subtask.isIndependent());
+  }
+
+  @Test
+  public void testAxiomNoInputs_withSubtask() {
+    String spec = "int a, b, x, y;\n [x -> y] -> b {m};";
+    AnnotatedClass ac = loadSpec(spec);
+    ClassRelation cr = assertClassRelation(ac, RelType.TYPE_METHOD_WITH_SUBTASK, vars(), vars("b"), "m");
+    assertEquals(1, cr.getSubtasks().size());
+    SubtaskClassRelation subtask = cr.getSubtasks().iterator().next();
+    assertClassRelation(subtask, RelType.TYPE_SUBTASK, vars("x"), vars("y"), null);
+    assertFalse("Should not be independent", subtask.isIndependent());
+  }
+
+  @Test
+  public void testAxiom_withTwoSubtask() {
+    String spec = "int a, b, x, y, u, v;\n [x -> y], [u -> v], a -> b {m};";
+    AnnotatedClass ac = loadSpec(spec);
+    ClassRelation cr = assertClassRelation(ac, RelType.TYPE_METHOD_WITH_SUBTASK, vars("a"), vars("b"), "m");
+    assertEquals(2, cr.getSubtasks().size());
+    Iterator<SubtaskClassRelation> iterator = cr.getSubtasks().iterator();
+    {
+      SubtaskClassRelation subtask = iterator.next();
+      assertClassRelation(subtask, RelType.TYPE_SUBTASK, vars("x"), vars("y"), null);
+      assertFalse("Should not be independent", subtask.isIndependent());
+    }
+    {
+      SubtaskClassRelation subtask = iterator.next();
+      assertClassRelation(subtask, RelType.TYPE_SUBTASK, vars("u"), vars("v"), null);
+      assertFalse("Should not be independent", subtask.isIndependent());
+    }
+  }
+
+  @Test(expected = SpecParseException.class)
+  public void testAxiom_withSubtaskBadOrder() {
+    String spec = "int a, b, x, y, u, v;\n a, [u -> v] -> b {m};";
+    loadSpec(spec);
+  }
+
+  @Test(expected = SpecParseException.class)
+  public void testAxiom_withTwoSubtaskBadOrder() {
+    String spec = "int a, b, x, y, u, v;\n [x -> y], a, [u -> v] -> b {m};";
+    loadSpec(spec);
+  }
+
+  @Test
+  public void testGoal_noInput() {
+    String spec = "int a, b;\n -> b;";
+    AnnotatedClass ac = loadSpec(spec);
+    assertClassRelation(ac, RelType.TYPE_UNIMPLEMENTED, vars(), vars("b"), null);
+  }
+
+  @Test
+  public void testGoal() {
+    String spec = "int a, b;\n a -> b;";
+    AnnotatedClass ac = loadSpec(spec);
+    assertClassRelation(ac, RelType.TYPE_UNIMPLEMENTED, vars("a"), vars("b"), null);
+  }
+
   @Test(expected = UnknownVariableException.class)
   public void testAxiom_UndeclaredVars() {
     String spec = "a -> b {methodName};";
