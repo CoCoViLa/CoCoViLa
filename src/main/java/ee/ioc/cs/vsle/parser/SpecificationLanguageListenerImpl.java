CoCoViLa/CoCoViLa

commit 9b29fcfe3f77515f96f6317909d8568ef2aaa882

Refactored spec parsing bootstrap: do not rely on "this", use real class name as root

src/main/java/ee/ioc/cs/vsle/synthesize/SpecParser.java
@@ -83,7 +83,7 @@ public class SpecParser {
      * @param text Secification text as String
      * @throws SpecParseException 
      */
-    static ArrayList<String> getSpec( String text, boolean isRefinedSpec ) throws IOException, SpecParseException {
+    static ArrayList<String> getSpec( String text, boolean isRefinedSpec ) {
         if ( !isRefinedSpec ) {
             text = refineSpec( text );
         }
@@ -269,7 +269,7 @@ public class SpecParser {
      * @param fileString a (Java) file containing the specification
      * @throws SpecParseException 
      */
-    private static String refineSpec( String fileString ) throws IOException, SpecParseException {
+    private static String refineSpec( String fileString ) {
         Matcher matcher;
 
         // remove comments before removing line brake \n
@@ -309,12 +309,11 @@ public class SpecParser {
         throw new SpecParseException( "Specification parsing error" );
     }
 
-    public ClassList parseSpecification( String fullSpec, String mainClassName, Set<String> schemeObjects )
-            throws IOException, SpecParseException, EquationException {
+    public ClassList parseSpecification( String fullSpec, String mainClassName, Set<String> schemeObjects ) {
 
         long start = System.currentTimeMillis();
         
-        ClassList classes = parseSpecificationImpl( refineSpec( fullSpec ), TYPE_THIS, schemeObjects,
+        ClassList classes = parseSpecificationImpl( refineSpec( fullSpec ), mainClassName, schemeObjects,
                 new LinkedHashSet<String>() );
 
         logger.info("Specification parsed in: " + (System.currentTimeMillis() - start) + "ms.");
@@ -349,8 +348,7 @@ public class SpecParser {
      *                check. Needed to prevent infinite loop in case of mutual
      *                declarations.
      */
-    private ClassList parseSpecificationImpl( String spec, String className, Set<String> schemeObjects,
-            Set<String> checkedClasses ) throws IOException, SpecParseException, EquationException {
+    private ClassList parseSpecificationImpl( String spec, String className, Set<String> schemeObjects, Set<String> checkedClasses ) {
         
         AnnotatedClass annClass = new AnnotatedClass( className );
 
@@ -740,8 +738,7 @@ public class SpecParser {
      * @throws SpecParseException 
      * @throws SpecParseException
      */
-    private boolean checkSpecClass( String parentClassName, Set<String> checkedClasses, ClassList classList,
-            String type ) throws IOException, SpecParseException {
+    private boolean checkSpecClass( String parentClassName, Set<String> checkedClasses, ClassList classList, String type ) {
 
         logger.debug( "Checking existence of " + packagePath + type + ".java" );
         

src/main/java/ee/ioc/cs/vsle/editor/ProgramRunner.java
@@ -45,18 +45,9 @@ import ee.ioc.cs.vsle.api.TerminateProgramException;
 import ee.ioc.cs.vsle.ccl.CCL;
 import ee.ioc.cs.vsle.ccl.CompileException;
 import ee.ioc.cs.vsle.event.EventSystem;
-import ee.ioc.cs.vsle.parser.SpecificationLoader;
-import ee.ioc.cs.vsle.synthesize.AnnotatedClass;
-import ee.ioc.cs.vsle.synthesize.ClassList;
-import ee.ioc.cs.vsle.synthesize.CodeGenerator;
-import ee.ioc.cs.vsle.synthesize.EquationException;
-import ee.ioc.cs.vsle.synthesize.LineErrorException;
-import ee.ioc.cs.vsle.synthesize.MutualDeclarationException;
-import ee.ioc.cs.vsle.synthesize.SpecParseException;
-import ee.ioc.cs.vsle.synthesize.SpecParser;
-import ee.ioc.cs.vsle.synthesize.Synthesizer;
-import ee.ioc.cs.vsle.synthesize.UnknownVariableException;
-import ee.ioc.cs.vsle.synthesize.Var;
+import ee.ioc.cs.vsle.parser.ParsedSpecificationContext;
+import ee.ioc.cs.vsle.parser.SpecParserUtil;
+import ee.ioc.cs.vsle.synthesize.*;
 import ee.ioc.cs.vsle.table.TableManager;
 import ee.ioc.cs.vsle.util.FileFuncs.FileSystemStorage;
 import ee.ioc.cs.vsle.util.FileFuncs.GenStorage;
@@ -265,10 +256,6 @@ public class ProgramRunner {
         try {
             foundVars.clear();
 
-            mainClassName = SpecParser.getClassName( fullSpec );
-
-            logger.info( "Computing " + mainClassName );
-
             Set<String> schemeObjects = new HashSet<String>();
 
             for (GObj gObj : schemeContainer.getObjectList()) {
@@ -276,26 +263,15 @@ public class ProgramRunner {
             }
             
             long start = System.currentTimeMillis();
-            
-            switch (RuntimeProperties.getSpecParserKind()) {
-            case REGEXP: {
-              classList = new SpecParser(schemeContainer.getWorkDir()).parseSpecification( fullSpec, mainClassName, schemeObjects );
-              break;
-            }
-            case ANTLR: {
-              SpecificationLoader specificationLoader = new SpecificationLoader(schemeContainer.getWorkDir(), schemeObjects);
-              specificationLoader.loadSpecification(fullSpec, TypeUtil.TYPE_THIS);
-              Collection<AnnotatedClass> loadedSpecifications = specificationLoader.getLoadedSpecifications();
-              classList = new ClassList();
-              classList.addAll(loadedSpecifications);
-              break;
-            }
-            }
-            
-            System.out.println("Done " + (System.currentTimeMillis() - start));
+
+            ParsedSpecificationContext parsedSpecificationContext = SpecParserUtil.parseSpec(fullSpec, schemeContainer.getWorkDir(), schemeObjects);
+            mainClassName = parsedSpecificationContext.mainClassName;
+            classList = parsedSpecificationContext.classList;
+
+            logger.info("Parsed '{}' in {}ms.", mainClassName, (System.currentTimeMillis() - start));
             getAssumptions().clear();
 
-            return Synthesizer.makeProgramText( fullSpec, computeAll, classList, mainClassName, this );
+            return Synthesizer.makeProgramText( parsedSpecificationContext, computeAll, this );
             
         } catch ( Throwable ex ) {
             reportException(ex);

src/main/java/ee/ioc/cs/vsle/parser/ParsedSpecificationContext.java
@@ -1,0 +1,18 @@
+package ee.ioc.cs.vsle.parser;
+
+import ee.ioc.cs.vsle.synthesize.ClassList;
+
+/**
+ * @author Pavel Grigorenko
+ */
+public class ParsedSpecificationContext {
+  public final String fullRootSpec;
+  public final ClassList classList;
+  public final String mainClassName;
+
+  public ParsedSpecificationContext(String fullRootSpec, ClassList classList, String mainClassName) {
+    this.fullRootSpec = fullRootSpec;
+    this.classList = classList;
+    this.mainClassName = mainClassName;
+  }
+}

src/main/java/ee/ioc/cs/vsle/parser/SpecParserUtil.java
@@ -1,0 +1,34 @@
+package ee.ioc.cs.vsle.parser;
+
+import ee.ioc.cs.vsle.editor.RuntimeProperties;
+import ee.ioc.cs.vsle.synthesize.AnnotatedClass;
+import ee.ioc.cs.vsle.synthesize.ClassList;
+import ee.ioc.cs.vsle.synthesize.SpecParser;
+
+import java.util.Set;
+
+/**
+ * @author Pavel Grigorenko
+ */
+public class SpecParserUtil {
+
+  public static ParsedSpecificationContext parseSpec(String spec, String workingDir, Set<String> schemeObjects) {
+    String mainClassName = null;
+    ClassList classList = null;
+    switch (RuntimeProperties.getSpecParserKind()) {
+      case REGEXP: {
+        mainClassName = SpecParser.getClassName(spec);
+        classList = new SpecParser(workingDir).parseSpecification( spec, mainClassName, schemeObjects );
+        break;
+      }
+      case ANTLR: {
+        SpecificationLoader specificationLoader = new SpecificationLoader(workingDir, schemeObjects);
+        AnnotatedClass _this = specificationLoader.loadSpecification(spec);
+        mainClassName = _this.getName();
+        classList = new ClassList(specificationLoader.getLoadedSpecifications());
+        break;
+      }
+    }
+    return new ParsedSpecificationContext(spec, classList, mainClassName);
+  }
+}

src/main/java/ee/ioc/cs/vsle/parser/SpecificationLoader.java
@@ -65,6 +65,10 @@ public class SpecificationLoader {
 		return annotatedClass;
 	}
 	
+	public AnnotatedClass loadSpecification(String specificationCode) {
+		return loadSpecification(specificationCode, null);
+	}
+
 	public AnnotatedClass loadSpecification(String specificationCode, String specificationName) {
 		AnnotatedClass annotatedClass = loadSpecification(new ANTLRInputStream(specificationCode), specificationName);
 		return annotatedClass;

src/main/java/ee/ioc/cs/vsle/synthesize/ProblemCreator.java
@@ -53,12 +53,16 @@ public class ProblemCreator {
         
         this.indpSubtasks = indpSubtasks;
     }
-    
+
     Problem makeProblem() throws SpecParseException {
+        return makeProblem(TYPE_THIS);
+    }
+
+    Problem makeProblem(String rootClassName) throws SpecParseException {
         
         long start = System.currentTimeMillis();
         
-        Problem problem = new Problem( new Var( new ClassField( TYPE_THIS, TYPE_THIS ), null ) );
+        Problem problem = new Problem( new Var( new ClassField( TYPE_THIS, rootClassName ), null ) );
         
         makeProblemImpl( problem.getRootVar(), problem, /*new HashMap<String, Integer>(),*/ true );
         

src/main/java/ee/ioc/cs/vsle/synthesize/Synthesizer.java
@@ -1,7 +1,5 @@
 package ee.ioc.cs.vsle.synthesize;
 
-import static ee.ioc.cs.vsle.util.TypeUtil.TYPE_THIS;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
@@ -12,6 +10,7 @@ import java.util.regex.Pattern;
 import ee.ioc.cs.vsle.api.ComputeModelException;
 import ee.ioc.cs.vsle.editor.ProgramRunner;
 import ee.ioc.cs.vsle.editor.RuntimeProperties;
+import ee.ioc.cs.vsle.parser.ParsedSpecificationContext;
 import ee.ioc.cs.vsle.util.FileFuncs;
 import ee.ioc.cs.vsle.util.FileFuncs.FileSystemStorage;
 import ee.ioc.cs.vsle.util.FileFuncs.GenStorage;
@@ -43,7 +42,7 @@ public class Synthesizer {
      * @throws SpecParseException 
     */
     public static void makeProgram( String progText, ClassList classes, String mainClassName, String path, GenStorage storage ) throws SpecParseException {
-        generateSubclasses( classes, path, storage );
+        generateSubclasses( mainClassName, classes, path, storage );
         storage.writeFile( mainClassName + ".java", progText );
     }
 
@@ -51,29 +50,24 @@ public class Synthesizer {
      * and planner and	returning compilable java source.
      * Creating a problem means parsing the specification(s recursively), unfolding it, and making
      * a flat representation of the specification (essentially a graph). Planning is run on this graph.
-     * @param fileString -
+     * @param context -
      * @param computeAll -
-     * @param classList -
-     * @param mainClassName -
      * @return String -
      * @throws SpecParseException -
      */
-    public static String makeProgramText( String fileString, boolean computeAll, ClassList classList,
-                                   String mainClassName, ProgramRunner runner ) throws SpecParseException {
+    public static String makeProgramText( ParsedSpecificationContext context, boolean computeAll, ProgramRunner runner ) throws SpecParseException {
 
-        Problem problem = null;
-        // call the packageParser to create a problem from the specification
-
-        problem = new ProblemCreator(classList).makeProblem();
+        // call the Problem Creator to create a problem from the specification
+        Problem problem = new ProblemCreator(context.classList).makeProblem(context.mainClassName);
             
         // run the planner on the obtained problem
         EvaluationAlgorithm algorithm = PlannerFactory.getInstance().getCurrentPlanner().invokePlaning( problem, computeAll );
         
         if( RuntimeProperties.isShowAlgorithm() ) {
-        	AlgorithmVisualizer.getInstance().addNewTab( mainClassName, algorithm );
+        	AlgorithmVisualizer.getInstance().addNewTab( context.mainClassName, algorithm );
         }
         
-        CodeGenerator cg = new CodeGenerator( algorithm, problem, mainClassName );
+        CodeGenerator cg = new CodeGenerator( algorithm, problem, context.mainClassName );
         String algorithmCode = cg.generate();
 
         if (runner != null) {
@@ -84,7 +78,7 @@ public class Synthesizer {
         StringBuilder prog = new StringBuilder();
         
         // start building the main source file.
-        AnnotatedClass ac = classList.getType( TYPE_THIS );
+        AnnotatedClass ac = context.classList.getType( context.mainClassName );
 
         // check all the fields and make declarations accordingly
         for ( ClassField field : ac.getClassFields() ) {
@@ -105,19 +99,20 @@ public class Synthesizer {
         
         prog.append( CodeGenerator.OT_TAB ).append( "public static void main( String[] args ) {\n" )
                 .append( CodeGenerator.OT_TAB ).append( CodeGenerator.OT_TAB )
-                .append( "new " ).append( mainClassName ).append( "().compute();\n" )
+                .append( "new " ).append( context.mainClassName ).append( "().compute();\n" )
                 .append( CodeGenerator.OT_TAB ).append( "}\n" );
         
         Pattern pattern;
         Matcher matcher;
 
         pattern = Pattern.compile("(?:public[ \t\n]+)?class[ \t\n]+" 
-        		+ mainClassName + "(?:[ \t\n]+extends[ \t\n]+([A-Za-z]\\w*))?");
-        
+        		+ context.mainClassName + "(?:[ \t\n]+extends[ \t\n]+([A-Za-z]\\w*))?");
+
+        String fileString = context.fullRootSpec;
         matcher = pattern.matcher( fileString );
 
         if ( matcher.find() ) {
-            fileString = matcher.replaceAll("public class " + mainClassName
+            fileString = matcher.replaceAll("public class " + context.mainClassName
             		+ (matcher.start(1) < matcher.end(1) 
             				? " extends " + matcher.group(1) : "")
             		+ " implements " + CodeGenerator.GENERATED_INTERFACE_NAME);
@@ -140,10 +135,11 @@ public class Synthesizer {
 
     /**
      Generates compilable java classes from the annotated classes that have been used in the specification.
-     @param classes List of classes obtained from the ee.ioc.cs.editor.synthesize.SpecParser
-     * @throws SpecParseException 
+     @throws SpecParseException
+      * @param mainClassName
+     * @param classes List of classes obtained from the ee.ioc.cs.editor.synthesize.SpecParser
      */
-    private static void generateSubclasses( ClassList classes, String path, GenStorage storage ) throws SpecParseException {
+    private static void generateSubclasses(String mainClassName, ClassList classes, String path, GenStorage storage) throws SpecParseException {
 
     	if( classes == null ) {
     		throw new SpecParseException( "Empty Class list!!!" );
@@ -156,7 +152,7 @@ public class Synthesizer {
         // for each class generate new one used in synthesis
         for ( AnnotatedClass pClass : classes ) {
             
-            if ( !pClass.getName().equals( TYPE_THIS ) ) {
+            if ( !pClass.getName().equals( mainClassName ) ) {
                 
                 fileString = FileFuncs.getFileContents(
                         new File(path, pClass.getName() + ".java"));
@@ -261,11 +257,10 @@ public class Synthesizer {
             String mainClassName = SpecParser.getClassName( file );
             
             ClassList classList = new SpecParser(path).parseSpecification(file, mainClassName, null);
-            String prog = makeProgramText( file, true, classList, mainClassName, null ); //changed to true
+            String prog = makeProgramText( new ParsedSpecificationContext(file, classList, mainClassName), true, null ); //changed to true
 
             String outputDir = RuntimeProperties.getGenFileDir();
-            makeProgram( prog, classList, mainClassName, path,
-                    new FileSystemStorage(outputDir));
+            makeProgram( prog, classList, mainClassName, path, new FileSystemStorage(outputDir));
         } catch ( UnknownVariableException uve ) {
             logger.error("Fatal error: variable " + uve.getMessage() + " not declared");
         } catch ( LineErrorException lee ) {

src/test/java/ee/ioc/cs/vsle/parser/AbstractParserTest.java
@@ -1,6 +1,7 @@
 package ee.ioc.cs.vsle.parser;
 
 import ee.ioc.cs.vsle.synthesize.*;
+import ee.ioc.cs.vsle.util.TypeUtil;
 import ee.ioc.cs.vsle.vclass.Alias;
 import ee.ioc.cs.vsle.vclass.ClassField;
 import org.antlr.v4.runtime.ANTLRInputStream;
@@ -233,11 +234,7 @@ public abstract class AbstractParserTest {
 
     @Override
     ClassList loadSpecs(String spec) {
-      try {
-        return specParser.parseSpecification(spec, null, null);
-      } catch (IOException e) {
-        throw new RuntimeException(e);
-      }
+      return specParser.parseSpecification(spec, TypeUtil.TYPE_THIS, null);
     }
 
     public boolean isRegexParser() { return true; }

commit e1fd8f4a9e3c1006c5a3a9ad85439c79e436ce29

Fixed regression made in 9b29fcf (Hydraulics package stopped working due to this)

src/main/java/ee/ioc/cs/vsle/parser/SpecificationLanguageListenerImpl.java
@@ -675,7 +675,7 @@ public class SpecificationLanguageListenerImpl extends SpecificationLanguageBase
 			classField.setStatic(isStatic);
 			annotatedClass.addField(classField);
 			//If object of currient type is on scheme
-            if ( isSpecificationClass() && TYPE_THIS.equals(annotatedClass.getName())  && specificationLoader.isSchemeObject(name)) {
+            if ( isSpecificationClass() && annotatedClass.getName().equals(specificationName)  && specificationLoader.isSchemeObject(name)) {
                 String s = name + "." + CodeGenerator.SPEC_OBJECT_NAME;
                 String meth = s + " = " + "\"" + name + "\"";
 

src/main/java/ee/ioc/cs/vsle/synthesize/SpecParser.java
@@ -57,6 +57,8 @@ public class SpecParser {
     private final SpecificationSourceProvider<String> specSourceProvider;
     private final String packagePath;
 
+    private String rootClassName;
+
     public SpecParser(String packagePath) {
         this.packagePath = packagePath;
         specSourceProvider = new FileSourceProvider();
@@ -311,6 +313,8 @@ public class SpecParser {
 
     public ClassList parseSpecification( String fullSpec, String mainClassName, Set<String> schemeObjects ) {
 
+        rootClassName = mainClassName;
+
         long start = System.currentTimeMillis();
         
         ClassList classes = parseSpecificationImpl( refineSpec( fullSpec ), mainClassName, schemeObjects,
@@ -433,7 +437,7 @@ public class SpecParser {
                             /* ****** SPEC_OBJECT_NAME ****** */
                             // add the following relation only if the object
                             // exists on a given scheme
-                            if ( schemeObjects != null && specClass && ( className == TYPE_THIS )
+                            if ( schemeObjects != null && specClass && ( className.equals(rootClassName) )
                                     && schemeObjects.contains( vars[ i ] ) ) {
                                 var.setSchemeObject(true);
                                 String s = vars[ i ] + "." + CodeGenerator.SPEC_OBJECT_NAME;